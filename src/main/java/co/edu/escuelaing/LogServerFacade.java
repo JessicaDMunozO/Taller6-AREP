503b705370592b9fe2ea3d3ef2485ac4005491bc
JessicaDMunozO <jessitae@gmail.com>
2024-03-10 15:19:36 -0500

    Docker

--- src/main/java/co/edu/escuelaing/LogServerFacade.java
+++ src/main/java/co/edu/escuelaing/LogServerFacade.java
@@ -3,10 +3,11 @@ package co.edu.escuelaing;
 import static spark.Spark.*;
 
 public class LogServerFacade {
-    private static final String[] LOG_SERVICE_URL = { "http://localhost:35001/logservice",
-            "http://localhost:35002/logservice", "http://localhost:35003/logservice" };
+    private static final String[] LOG_SERVICE_URL = { "http://logservice1:35000/logservice",
+            "http://logservice2:35000/logservice", "http://logservice3:35000/logservice" };
 
     public static void main(String[] args) {
+        port(getPort());
         RemoteLogServiceInvoker invoker = new RemoteLogServiceInvoker(LOG_SERVICE_URL);
 
         staticFiles.location("/public");
@@ -16,5 +17,13 @@ public class LogServerFacade {
             String param = req.queryParams("param");
             return invoker.invoke(param);
         });
+
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 46000;
     }
 }
